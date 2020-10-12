a54c98d1074c8277851d5473a060533a973615a0
opilane <opilane@HP-CZC6297B92>
2020-10-12 14:33:29 +0300

    Add project files.

---
+++ Kalender/Kalender/Osenj.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kalender
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Osenj : TabbedPage
+    {
+        public Osenj()
+        {
+            InitializeComponent();
+        }
+    }
+}
