429f09fa4495a82d9d31b09b0d67d47d1a0c4b0e
opilane <opilane@HP-CZC6297B92>
2020-10-19 15:00:57 +0300

    kalender

--- Kalender/Kalender/Osenj.xaml.cs
+++ Kalender/Kalender/Osenj.xaml.cs
@@ -16,5 +16,18 @@ namespace Kalender
         {
             InitializeComponent();
         }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("начало войны", "1 сентября началась Вторая Мировая война", "välja");
+        }
+        private async void Button_Clicked1(object sender, EventArgs e)
+        {
+            await DisplayAlert("День учителей", "День учителей и наставников в Узбекистане  Праздники Узбекистана. У каждого из нас есть свой Учитель и свой Наставник. Это люди, без которых мы не состоялись бы как личности, не получили бы жизненно важных навыков, знаний и умений.", "välja");
+        }
+        private async void Button_Clicked2(object sender, EventArgs e)
+        {
+            await DisplayAlert("Сталинград", "19 ноября 1942 года в ходе Великой Отечественной войны началась одна из крупнейших стратегических операций советских войск под кодовым названием «Уран». Началось контрнаступление в районе Сталинграда.", "välja");
+        }
+
     }
 }
--- Kalender/Kalender/Vesna.xaml.cs
+++ Kalender/Kalender/Vesna.xaml.cs
@@ -16,5 +16,17 @@ namespace Kalender
         {
             InitializeComponent();
         }
+        private async void Button_Clicked9(object sender, EventArgs e)
+        {
+            await DisplayAlert("март", "1 месяц лета", "välja");
+        }
+        private async void Button_Clicked10(object sender, EventArgs e)
+        {
+            await DisplayAlert("мой др", "12 Апреля день рождения великиго императора Владислава Лялика", "välja");
+        }
+        private async void Button_Clicked11(object sender, EventArgs e)
+        {
+            await DisplayAlert("день победы", "9 мая завершилась Вторая Мировая война", "välja");
+        }
     }
 }
